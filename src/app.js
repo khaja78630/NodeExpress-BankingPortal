810241cfb69e9a050b2f61f1b69b47b7d9a9ef4e
khaja <khaja78630@gmail.com>
2020-07-05 21:37:29 +0530

    module2 code changes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,45 @@ app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+//Code start
+const accountData = fs.readFileSync('src/json/accounts.json','UTF8');
+const accounts= JSON.parse(accountData);
+//Code end
+
+//Code start
+const userData = fs.readFileSync('src/json/users.json','UTF8');
+const users= JSON.parse(userData);
+//Code end
+
+
 app.get("/", function (req, res) {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  res.render("index", { title: "Account Summary", accounts : accounts});
 });
+
+app.get('/savings',function(req,res){
+  res.render("account", { account : accounts.savings });
+});
+
+
+//code starts
+app.get('/checking',function(req,res){
+  res.render("account", { account : accounts.checking });
+});
+
+app.get('/credit',function(req,res){
+  res.render("account", { account : accounts.credit });
+});
+
+//code end
+
+
+//Code start
+app.get('/profile',function(req,res){
+  res.render("profile", { user : users[0] });
+});
+//Code end
+
+
 
 app.listen(3000, function () {
   console.log("Node server is running..");
